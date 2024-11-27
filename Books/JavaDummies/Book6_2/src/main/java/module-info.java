ca6da9244401676da927c2f3526c83adf5a5db01
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2024-11-27 20:28:14 +0200

    Add files via upload

---
+++ Books/JavaDummies/Book6_2/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.book6_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.book6_2 to javafx.fxml;
+    exports com.example.book6_2;
+}
